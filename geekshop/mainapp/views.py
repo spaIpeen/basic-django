50306199f242f309362f8065435eac66c2aedf69
spalpeen <oxybimboo@gmail.com>
2021-12-01 17:36:43 +0200

    Revert "lesson_1"
    This reverts commit 4f487c9a234239defeb45b1f45ad24d4464f0d5f.

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,15 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-
-def main(request):
-    return render(request, 'mainapp/index.html')
-
-
-def products(request):
-    return render(request, 'mainapp/products.html')
-
-
-def contact(request):
-    return render(request, 'mainapp/contact.html')
