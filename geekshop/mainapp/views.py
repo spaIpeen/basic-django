cf2b9fab33728362b29d625c03c80b244ef8a87f
spalpeen <oxybimboo@gmail.com>
2021-12-03 12:29:39 +0200

    homework lesson_2 done

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,15 +1,36 @@
 from django.shortcuts import render
+import pathlib
+import json
 
 # Create your views here.
 
+with open(f'{pathlib.Path().absolute()}/mainapp/templates/mainapp/json/menu.json', 'r') as read_file:
+    links_menu = json.load(read_file)
+
+with open(f'{pathlib.Path().absolute()}/mainapp/templates/mainapp/json/categories.json', 'r') as read_file:
+    links_categories_menu = json.load(read_file)
+
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    content = {
+        'title': 'Домой',
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    content = {
+        'title': 'Продукты',
+        'links_categories_menu': links_categories_menu,
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    content = {
+        'title': 'Контакты',
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/contact.html', content)
